a39a5ad75d8a65300f209b3ed1229194c05c3659
andreyskarpov <77676716+andreyskarpov@users.noreply.github.com>
2022-05-14 16:02:08 +0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,81 +1,102 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import altair as alt
 
 with st.echo(code_location='below'):
-    """
-    ## Hello, World!
-    """
-
-
-    def print_hello(name="World"):
-        st.write(f"### Hello, {name}!")
-
-
-    name = st.text_input("Your name", key="name", value="Anonymous")
-    print_hello(name)
-
-    """
-    ## Добавим графики
-    Чтобы заработали библиотеки seaborn и altair, нужно добавить в проект файл 
-    `requirements.txt` с такими строчками:
-    
-        seaborn
-        altair
-    """
-
-
-    a = st.slider("a")
-    x = np.linspace(-6, 6, 500)
-    df = pd.DataFrame(dict(x=x, y=np.sin(a * x)))
-    fig, ax = plt.subplots()
-    sns.lineplot(data=df, x="x", y="y", ax=ax)
-    st.pyplot(fig)
-
-    """
-    ## Немного анализа данных
-    """
-
+    def graph(x):
+        fig=px.histogram(df[df['Branch']==x][['Month','Total']]
+                                 .groupby('Month')
+                                 .sum()
+                                 .reset_index()
+                                 , x="Month",y='Total', histfunc='sum', width=300, height=400,
+                         labels=dict(Month="Месяц", Total ="Сумма продаж"))\
+            .update_xaxes(categoryorder='array', categoryarray=['January','February', 'March'],)
+        return fig
 
     @st.cache
     def get_data():
         data_url = (
-            "https://github.com/Godoy/imdb-5000-movie-dataset/raw/"
-            "master/data/movie_metadata.csv"
+            "https://raw.githubusercontent.com/sushantag9/Supermarket-Sales-Data-Analysis/master/supermarket_sales%20-%20Sheet1.csv"
         )
         return (
             pd.read_csv(data_url)
-            .dropna(subset=["title_year"])
-            .assign(
-                title_year=lambda x: pd.to_datetime(
-                    x["title_year"], format="%Y"
+                .dropna(subset=["Date"])
+                .assign(
+                Date=lambda x: pd.to_datetime(
+                    x["Date"]
                 )
             )
         )
 
 
-    df = get_data()
+    sns.set_palette("Paired")
+    df=get_data()
+    df['Month'] = df['Date'].dt.month_name()
+    df['Weekday'] = df['Date'].dt.weekday
+    """
+    ## Привет! Сегодня мы посмотрим на данные о продажах в трех магазинах
+    """
 
-    director = st.selectbox(
-        "Director", df["director_name"].value_counts().iloc[:10].index
-    )
+    """
+    ### Для начала посмотрим на общую картину продаж
+    """
 
-    df_selection = df[lambda x: x["director_name"] == director]
-    df_selection
+    a=df[['Total', 'Date']].groupby(['Date']).agg({'Total': ['sum', 'mean']})['Total'].reset_index()
+    fig1 = px.line(a, x="Date", y=['sum','mean'], title='Cумма продаж по 3-м магазинам',
+                   labels=dict(Date="Дата", Total ="Сумма продаж"))
 
-    chart = (
-        alt.Chart(df_selection)
-        .mark_circle()
-        .encode(x=alt.X("title_year:T"), y="imdb_score", tooltip="movie_title")
-    )
+    st.plotly_chart(fig1)
+    ### основан на FROM : (https://docs.streamlit.io/library/api-reference/layout/st.columns)
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Магазин А")
+        st.plotly_chart(graph('A'))
 
-    st.altair_chart(
-        (
-            chart
-            + chart.transform_loess("title_year", "imdb_score").mark_line()
-        ).interactive()
-        # .transform_loess добавляет сглаживающую кривую
-    )
+    with col2:
+        st.header("Магазин B")
+        st.plotly_chart(graph('B'))
+
+    with col3:
+        st.header("Магазин C")
+        st.plotly_chart(graph('C'))
+    ### END FROM
+    fig3= px.pie(df[['Branch','Total']].groupby('Branch').sum().reset_index(), values='Total',names='Branch',  title='Соотношение продаж по магазинам', hole=.3 )
+    st.plotly_chart(fig3)
+
+    fig2=px.treemap(df[['Branch','Product line', 'Total','Gender']]
+                    .groupby(['Branch','Product line','Gender'])
+                    .sum()
+                    .reset_index(),
+                    path=['Branch','Product line','Gender'],
+                    values='Total',
+                    color='Product line',
+                    title='Разбивка по категориям в каждом магазине (Интерактивный)',
+                    width = 800, height=600)
+
+    st.plotly_chart(fig2)
+
+    """
+    ### Можно посмотреть на различные корреляции
+    """
+    col1, col2=st.columns(2)
+    with col1:
+        x1=st.selectbox('Выберите 1 показатель', ['Пол', 'Категория', 'Метод оплаты'])
+    dict={'Пол':'Gender', 'Категория':'Product line', 'Метод оплаты':'Payment'}
+    with col2:
+        x2=st.selectbox('Выберите 2 показатель', ['Пол', 'Категория', 'Метод оплаты'])
+    if x1==x2:
+        st.write('Выберите разные показатели')
+    else:
+        data1=pd.pivot_table(df[['Total',dict[x1],dict[x2]]], index=dict[x1], columns=dict[x2], aggfunc=np.sum)
+        fig, ax = plt.subplots()
+        sns.heatmap(data1, ax=ax )
+        st.pyplot(fig)
+    """
+    ### Теперь посмотрим на разброс значений чеков в зависимости от дня недели
+    """
+    fig2, ax2 = plt.subplots()
+    sns.boxplot(x='Weekday', y='Total', hue='Gender', data=df[['Weekday','Total','Gender']], ax=ax2)
+    st.pyplot(fig2)
